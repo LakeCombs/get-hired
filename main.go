899b2c58bf19cd9ed10120791bb0ac88c0561437
LakeCombs <adesolaolamilekan2@gmail.com>
2022-09-04 17:47:02 +0000

    added authentication

--- main.go
+++ main.go
@@ -1,71 +1,32 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"gethired/controller"
-	"gethired/services"
-	"log"
+	"gethired/routes"
+	"os"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
-
-var (
-	server         *gin.Engine
-	userservice    services.UserService
-	usercontroller controller.UserController
-	ctx            context.Context
-	usercollection *mongo.Collection
-	mongoclient    *mongo.Client
-	err            error
-)
-
-func init() {
-	ctx = context.TODO()
-	mongocon := options.Client().ApplyURI("mongodb://localhost:27017")
-	mongoclient, err := mongo.Connect(ctx, mongocon)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = mongoclient.Ping(ctx, readpref.Primary())
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("mongo connection establish")
-
-	usercollection = (mongoclient.Database("userdb")).Collection("users")
-	userservice = services.NewUserService(usercollection, ctx)
-	server = gin.Default()
-}
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 
-	// ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	// client, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
-	// if err != nil {
-	// 	println(err)
-	// 	panic(err)
-	// }
+	router := gin.New()
+	router.Use(gin.Logger())
 
-	defer mongoclient.Disconnect(ctx)
-	basepath := server.Group("/v1")
-	usercontroller.RegisterUserRoutes(basepath)
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
 
-	log.Fatal(server.Run(":9090"))
+	router.GET("/api/v1", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "Acesss granted for api-v1"})
 
-	// r := gin.Default()
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": "pong",
-	// 	})
-	// })
+	})
 
-	// r.Run()
+	router.GET("/api/v2", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "Access granted for api-v2"})
+	})
+
+	router.Run(":" + port)
 }
